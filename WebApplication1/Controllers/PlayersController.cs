caa25485bd5b27c0e49aa653ed01598772ec9b35
Drus566 <ahdpeu566@mail.ru>
2020-03-08 18:42:37 +0500

    init

---
+++ WebApplication1/Controllers/PlayersController.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WebApplication1.Models;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace WebApplication1.Controllers
+{
+    public class PlayersController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        [BindProperty]
+        public Player Player { get; set; }
+        [BindProperty]
+        public Player Team { get; set; }
+        public PlayersController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        
+        // GET: Players
+        public IActionResult Index()
+        {
+            var players = _db.Players.ToList();
+            if (players != null)
+            {
+                foreach (Player p in players)
+                {
+                    _db.Teams.Where(t => t.TeamId == p.TeamId).Load();
+                }
+                return View(players);
+            }
+            return NotFound();
+        }
+
+        public IActionResult Upsert(int? id)
+        {
+            var teams = _db.Teams.ToList();
+            ViewBag.Teams = new SelectList(teams, "TeamId", "Name");
+
+            // create
+            if (id == null)
+            {
+                Player = new Player();
+                return View(Player);
+            }
+            // update
+            Player = _db.Players.FirstOrDefault(u => u.Id == id);
+            if (Player == null)
+            {
+                return NotFound();
+            }
+            return View(Player);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Upsert()
+        {
+            if (ModelState.IsValid)
+            {
+             
+                if (Player.Id == 0)
+                {
+                    //create
+                    _db.Players.Add(Player);
+                }
+                else
+                {
+                    _db.Players.Update(Player);
+                }
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(Player);
+        }
+    }
+}
---
+++ WebApplication1/Models/ApplicationDbContext.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Models
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Player> Players { get; set; }
+        public DbSet<Team> Teams { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder
+                .Entity<Player>()
+                .Property(p => p.Gender)
+                .HasConversion(
+                    v => v.ToString(),
+                    v => (Gender)Enum.Parse(typeof(Gender), v));
+
+            modelBuilder
+                .Entity<Player>()
+                .Property(p => p.Country)
+                .HasConversion(
+                    v => v.ToString(),
+                    v => (Country)Enum.Parse(typeof(Country), v));
+        }
+    }
+}
