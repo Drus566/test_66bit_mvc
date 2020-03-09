7e830b8ceae74f14886b93d918e934f4956853a5
Drus566 <ahdpeu566@mail.ru>
2020-03-09 09:48:00 +0500

    Work with signalr

--- WebApplication1/wwwroot/js/chat.js
+++ WebApplication1/wwwroot/js/chat.js
@@ -3,36 +3,47 @@
 var connection = new signalR.HubConnectionBuilder().withUrl("/chatHub").build();
 
 connection.on("ReceiveMessage", function (id, name, surname, birth, team, gender, country) {
-    var man = document.getElementById(id);
+    // Find Players table
+    if (document.getElementById("PlayersList")) {
+        // Find Player
+        var man = document.getElementById(id);
+        // If exists, then update, else add new player
+        if (man) {
+            man.getElementsByClassName("1")[0].innerHTML = name;
+            man.getElementsByClassName("2")[0].innerHTML = surname;
+            man.getElementsByClassName("3")[0].innerHTML = gender;
+            man.getElementsByClassName("4")[0].innerHTML = birth;
+            man.getElementsByClassName("5")[0].innerHTML = team;
+            man.getElementsByClassName("6")[0].innerHTML = country;
+        } else {
+            var tr = document.createElement("tr");
+            var tbody = document.querySelector("tbody");
 
-    if (man) {
-        man.getElementsByClassName("Name")[0].innerHTML = name;
-        man.getElementsByClassName("Surname")[0].innerHTML = surname;
-        man.getElementsByClassName("Gender")[0].innerHTML = gender;
-        man.getElementsByClassName("Birth")[0].innerHTML = birth;
-        man.getElementsByClassName("Team")[0].innerHTML = team;
-        man.getElementsByClassName("Country")[0].innerHTML = country;
-    } else {
-        var tr = document.createElement("tr");
-        tr.setAttribute('id', id);
-        tr.className = "text-center";
+            tr.setAttribute('id', id);
+            tr.className = "text-center";
 
-        [name, surname, gender, birth, team, country].forEach(function (item) {
-            var td = document.createElement("td");
-            td.className = getName(item)[0].ToUpperCase();
-            td.innerText = item;
+            [name, surname, gender, birth, team, country].forEach(function (item, index) {
+                var td = document.createElement("td");
 
-            tr.insertAdjacentElement('afterend', td);
-        });
+                td.className = `${index + 1}`;
+                td.innerText = item;
 
-        //var td = document.createElement("td");
-        //var a = document.createElement("a")
-        //<td>
-        //    <a asp-action="Upsert" asp-route-id="@p.Id" asp-controller="Players" class="btn btn-outline-dark form-control">
-        //        Edit
-        //    </a>
-        //</td>
+                tr.append(td);
+            });
 
+            var editWrapper = document.createElement('td');
+            var edit = document.createElement('a');
+
+            edit.className = "btn btn-outline-dark form-control";
+            edit.setAttribute("href", "/Players/Upsert/" + id);
+            edit.innerText = "Edit";
+
+            editWrapper.append(edit);
+
+            tr.append(editWrapper);
+
+            tbody.append(tr);
+        }
     }
 });
 
